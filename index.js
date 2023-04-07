b566514e36500053c6d0e22f53e33edbcdc24bd5
sulaimanadamu <adamusulaiman2021@gmail.com>
2023-04-07 23:24:50 +0100

    added a new feature, which changes light or dark mode

--- index.js
+++ index.js
@@ -17,5 +17,51 @@ function generatePassword(){
     }
    out1.textContent = pwd1
    out2.textContent = pwd2
+   console.log(document.getElementById("btn-flow").style.justifyContent)
+
 }
 
+    let right = true;
+    let left = false;
+
+  function modeBtn(){
+    if (right === true){
+    document.getElementById("mode-btn").innerHTML = "<i class='fas fa-sharp fa-solid fa-moon'></i>"
+    document.getElementById("btn-flow").style.justifyContent = "start";
+    document.getElementById("container").style.backgroundColor = "#1F2937"
+    document.getElementById("header").style.color = "#FFFFFF"
+    document.getElementById("random-text").style.color = "#4ADF86"
+    document.getElementById("sub-text").style.color = "#D5D4D8"
+    document.getElementById("password-input").style.color = "#2F3E53"
+
+    right = false
+    left =true
+    console.log("left")
+    }
+
+    else{
+        document.getElementById("btn-flow").style.justifyContent = "end";
+       document.getElementById("mode-btn").innerHTML = "<i class='fas fa-thin fa-sun'></i>"
+       document.getElementById("container").style.backgroundColor = "#ECFDF5"
+       document.getElementById("header").style.color = "#2B283A"
+       document.getElementById("random-text").style.color = "#10B981"
+       document.getElementById("sub-text").style.color = "#6B7280"
+       document.getElementById("password-input").style.color = "#D5D4D8"
+
+       
+        right = true
+        left = false
+        console.log("right")
+    }
+
+
+}
+let text = ""
+function copyFirstPassword(){
+   text = document.getElementById("copybtn1")
+   navigator.clipboard.writeText(text)
+}
+function copySecondPassword(){
+    text = document.getElementById("copybtn2")
+    navigator.clipboard.writeText(text)
+}
